241800da015d44728430756929c3e65979de623a
Seppe Lenders <seppe.lenders@gmail.com>
2025-01-14 17:07:37 +0100

    add tests for new geometry functions

--- next/src/lib/util/geometry.test.ts
+++ next/src/lib/util/geometry.test.ts
@@ -74,7 +74,51 @@ describe('intersectPlanes', () => {
 });
 
 describe('intersectPlaneAndLine', () => {
-  throw new Error('Not implemented');
+  test('should return the intersection of a plane and a line', () => {
+    const plane = new THREE.Plane(new THREE.Vector3(1, 2, 3), 4);
+    const line = new THREE.Line3(
+      new THREE.Vector3(10, 10, 10),
+      new THREE.Vector3(11, 12, 13),
+    );
+
+    const intersection = geometry.intersectPlaneAndLine(
+      plane,
+      line,
+      new THREE.Vector3(),
+    );
+    expectToBeDefined(intersection);
+    expectVectorsToBeClose(intersection, new THREE.Vector3(5.43, 0.86, -3.71));
+  });
+
+  test("should return null if the plane and line don't intersect", () => {
+    const plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+    const line = new THREE.Line3(
+      new THREE.Vector3(0, 2, 0),
+      new THREE.Vector3(1, 2, 0),
+    );
+
+    const intersection = geometry.intersectPlaneAndLine(
+      plane,
+      line,
+      new THREE.Vector3(),
+    );
+    expect(intersection).toBeNull();
+  });
+
+  test('should return the start point of the line if the plane and line are coplanar', () => {
+    const plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+    const line = new THREE.Line3(
+      new THREE.Vector3(0, 2, 0),
+      new THREE.Vector3(1, 2, 0),
+    );
+
+    const intersection = geometry.intersectPlaneAndLine(
+      plane,
+      line,
+      new THREE.Vector3(),
+    );
+    expect(intersection).toBeNull();
+  });
 });
 
 describe('distanceToLine', () => {
@@ -133,10 +177,116 @@ describe('distanceToLine', () => {
   });
 });
 
+describe('distanceBetweenLines', () => {
+  test('should return the distance between non-parallel lines', () => {
+    const line1 = new THREE.Line3(
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(1, 0, 0),
+    );
+    const line2 = new THREE.Line3(
+      new THREE.Vector3(0, 1, 0),
+      new THREE.Vector3(0, 1, 1),
+    );
+
+    const distance = geometry.distanceBetweenLines(line1, line2);
+    expect(distance).toBeCloseTo(1);
+  });
+
+  test('should return the distance between parallel lines', () => {
+    const line1 = new THREE.Line3(
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(1, 0, 0),
+    );
+    const line2 = new THREE.Line3(
+      new THREE.Vector3(0, 2, 0),
+      new THREE.Vector3(1, 2, 0),
+    );
+
+    const distance = geometry.distanceBetweenLines(line1, line2);
+    expect(distance).toBeCloseTo(2);
+  });
+
+  test('should return the distance between coinciding lines', () => {
+    const line1 = new THREE.Line3(
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(1, 0, 0),
+    );
+    const line2 = new THREE.Line3(
+      new THREE.Vector3(10, 0, 0),
+      new THREE.Vector3(-1, 0, 0),
+    );
+
+    const distance = geometry.distanceBetweenLines(line1, line2);
+    expect(distance).toBeCloseTo(0);
+  });
+
+  test('should return the distance between intersecting lines', () => {
+    const line1 = new THREE.Line3(
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(1, 0, 0),
+    );
+    const line2 = new THREE.Line3(
+      new THREE.Vector3(0, 5, 0),
+      new THREE.Vector3(0, 1, 0),
+    );
+
+    const distance = geometry.distanceBetweenLines(line1, line2);
+    expect(distance).toBeCloseTo(0);
+  });
+});
+
 describe('vectorsAreParallel', () => {
-  throw new Error('Not implemented');
+  test('should return true for parallel vectors', () => {
+    const vector1 = new THREE.Vector3(1, 0, 0);
+    const vector2 = new THREE.Vector3(2, 0, 0);
+    expect(geometry.vectorsAreParallel(vector1, vector2)).toBe(true);
+  });
+
+  test('should return false for non-parallel vectors', () => {
+    const vector1 = new THREE.Vector3(1, 0, 0);
+    const vector2 = new THREE.Vector3(0, 1, 0);
+    expect(geometry.vectorsAreParallel(vector1, vector2)).toBe(false);
+  });
+
+  test('should return true for vectors that are parallel within a tolerance', () => {
+    const vector1 = new THREE.Vector3(1, 0, 0);
+    const vector2 = new THREE.Vector3(1, 0.0001, 0);
+    expect(geometry.vectorsAreParallel(vector1, vector2)).toBe(true);
+  });
+
+  test('should return false for vectors that are not parallel within a tolerance', () => {
+    const vector1 = new THREE.Vector3(1, 0, 0);
+    const vector2 = new THREE.Vector3(1, 0.01, 0);
+    console.log(new THREE.Vector3().crossVectors(vector1, vector2).lengthSq());
+
+    expect(geometry.vectorsAreParallel(vector1, vector2)).toBe(false);
+  });
+
+  test('should return true for vectors that are anti-parallel', () => {
+    const vector1 = new THREE.Vector3(1, 0, 0);
+    const vector2 = new THREE.Vector3(-1, 0, 0);
+    expect(geometry.vectorsAreParallel(vector1, vector2)).toBe(true);
+  });
 });
 
 describe('isAxis', () => {
-  throw new Error('Not implemented');
+  test('should return "x" for the X axis', () => {
+    const vector = new THREE.Vector3(1, 0, 0);
+    expect(geometry.isAxis(vector)).toBe('x');
+  });
+
+  test('should return "y" for the negative Y axis', () => {
+    const vector = new THREE.Vector3(0, -1, 0);
+    expect(geometry.isAxis(vector)).toBe('y');
+  });
+
+  test('should return "z" for the non-unit Z axis', () => {
+    const vector = new THREE.Vector3(0, 0, 2);
+    expect(geometry.isAxis(vector)).toBe('z');
+  });
+
+  test('should return null for a non-axis vector', () => {
+    const vector = new THREE.Vector3(1, 1, 0);
+    expect(geometry.isAxis(vector)).toBeNull();
+  });
 });
